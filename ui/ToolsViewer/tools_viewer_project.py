3dfc9d55222902de05195107b9b3925ffff36850
Tu Nguyen Anh Tuan <tu.nguyen_b@virtuosgames.com>
2023-08-03 10:59:44 +0700

    toolsviewer sync up pt.2

--- ui/ToolsViewer/tools_viewer_project.py
+++ ui/ToolsViewer/tools_viewer_project.py
@@ -2,6 +2,8 @@ import dearpygui.dearpygui as dpg
 from ui.ToolsViewer.utils import tkinter_file_dialog
 from multiprocessing import Queue
 from core.utils import create_queueHandler_logger, json_load_from_file_path
+from core.data_loader import refresh_core_data_with_json_dict
+from core.executor import execute_event
 from collections import OrderedDict
 from pathlib import Path
 
@@ -66,15 +68,29 @@ class ToolsViewer:
             self._open_tool(tool_name)
 
     def _open_tool(self, tool_name):
-        new_tab_id = self._init_new_tab(tool_name)
-        self._import_tool_to_tab(new_tab_id, tool_name)
+        tab_window_id = self._init_new_tab_and_get_main_tab_window_id(tool_name)
+        self._import_tool_to_tab(tab_window_id, self._tab_dict[tool_name])
 
-    def _init_new_tab(self, new_tab_name: str):
-        new_tab_id = dpg.add_tab(label=new_tab_name, parent=self.tab_bar_id, closable=False)
-        return new_tab_id
+    def _init_new_tab_and_get_main_tab_window_id(self, new_tab_name: str) -> int:
+        with dpg.tab(label=new_tab_name, parent=self.tab_bar_id, closable=False):
+            with dpg.child_window(label="Main Window",
+                                  height=self._setting_dict['viewport_height'] - 200,
+                                  autosize_x=True) as tab_window_id:
+                pass
+        return tab_window_id
 
-    def _import_tool_to_tab(self, tab_id: int, tool_name: str):
-        tool_data = json_load_from_file_path(self._tab_dict[tool_name])
+    def _import_tool_to_tab(self, tab_window_id: int, tool_relative_path: str):
+        tool_abs_path = Path(__file__).parent / tool_relative_path
+        tool_data = json_load_from_file_path(Path(__file__).parent / tool_abs_path)
+        self._add_user_input_boxes_to_tab_from_vars_data(tab_window_id, tool_data['vars'])
+        self._add_event_buttons_to_tab_from_nodes_data(tab_window_id, tool_data['nodes'])
+
+    def _add_user_input_boxes_to_tab_from_vars_data(self, tab_window_id: int, vars_data: dict):
+        # for var_info in vars_data.values():
+        pass
+    
+    def _add_event_buttons_to_tab_from_nodes_data(self, tab_window_id: int, nodes_data: dict):
+        pass
 
     def _update_project_data(self, project_path: Path):
         self._update_project_name(project_path.name)
@@ -100,21 +116,11 @@ class ToolsViewer:
                     with dpg.child_window(label="Main Window",
                                           height=self._setting_dict['viewport_height'] - 200,
                                           autosize_x=True):
-                        for each in input_var:
-                            if each['button_name']:
-                                dpg.add_button(label=each['button_name'],
-                                               tag=each['id'] + 'Event',
-                                               width=-1,
-                                               height=self._setting_dict['button_height'],
-                                               # callback=self.trigger_event,
-                                               user_data=each['id']
-                                               )
-                                with dpg.tooltip(each['id'] + 'Event'):
-                                    dpg.add_text('A tool tip of {}!'.format(each['id']))
-                    with dpg.child_window(label="Log Window",
-                                          autosize_x=True,
-                                          horizontal_scrollbar=True):
-                        dpg.add_text(tag='log', tracked=False, track_offset=1.0)
+                        pass
+            with dpg.child_window(label="Log Window",
+                                  autosize_x=True,
+                                  horizontal_scrollbar=True):
+                dpg.add_text(tag='log', tracked=False, track_offset=1.0)
 
     def callback_tab_bar_change(self, sender, app_data):
         pass
